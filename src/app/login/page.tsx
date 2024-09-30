d2ddf5f58d66649153ee87e7d5434b1011acfc87
karthikmoulya <karthikmoulya1995@gmail.com>
2024-09-30 10:08:31 +0530

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,119 @@
+'use client';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post('/api/users/login', user);
+      console.log('Login success', response.data);
+      toast.success('Login success');
+      router.push('/profile');
+    } catch (error: any) {
+      console.log('Login failed', error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <div className='flex min-h-full flex-col justify-center px-6 py-12 lg:px-8'>
+      <div className='sm:max-auto sm:w-full sm:max:-w-sm'>
+        <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+          {loading ? 'Processing' : 'Login'}
+        </h2>
+      </div>
+
+      <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+        <form
+          className='space-y-6'
+          action='#'
+          method='POST'
+          onSubmit={(e) => {
+            e.preventDefault();
+            onLogin();
+          }}
+        >
+          <div>
+            <label
+              htmlFor='email'
+              className='block text-sm font-medium leading-6 text-gray-900'
+            >
+              Email
+            </label>
+            <div className='mt-2'>
+              <input
+                id='email'
+                name='email'
+                type='email'
+                autoComplete='email'
+                required
+                value={user.email}
+                className='block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor='password'
+              className='block text-sm font-medium leading-6 text-gray-900'
+            >
+              Password
+            </label>
+            <div className='mt-2'>
+              <input
+                id='password'
+                name='password'
+                type='password'
+                autoComplete='password'
+                required
+                value={user.password}
+                className='block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              disabled={buttonDisabled}
+              type='submit'
+              className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+            >
+              {buttonDisabled ? 'No Login' : 'Login'}
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <Link
+        className='mt-10 text-center text-sm text-indigo-600 hover:text-indigo-500'
+        href={'/signup'}
+      >
+        Vist Signup Page
+      </Link>
+    </div>
+  );
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,142 @@
+'use client';
+import axios from 'axios';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+
+export default function SignupPage() {
+  const router = useRouter();
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+    username: '',
+  });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onSignUp = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post('/api/users/signup', user);
+      console.log('signup success', response.data);
+      router.push('/login'); // Redirection
+    } catch (error: any) {
+      console.log('Signup failed', error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+  return (
+    <div className='flex min-h-full flex-col justify-center px-6 py-12 lg:px-8'>
+      <div className='sm:max-auto sm:w-full sm:max:-w-sm'>
+        <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+          {loading ? 'Processing' : 'Signup'}
+        </h2>
+      </div>
+
+      <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+        <form
+          className='space-y-6'
+          action='#'
+          method='POST'
+          onSubmit={(e) => {
+            e.preventDefault(); // Prevent default form submission
+            onSignUp(); // Call signup logic
+          }}
+        >
+          <div>
+            <label
+              htmlFor='username'
+              className='block text-sm font-medium leading-6 text-gray-900'
+            >
+              Username
+            </label>
+            <input
+              className='block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+              id='username'
+              type='text'
+              value={user.username}
+              required
+              name='username'
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor='email'
+              className='block text-sm font-medium leading-6 text-gray-900'
+            >
+              Email
+            </label>
+            <div className='mt-2'>
+              <input
+                id='email'
+                name='email'
+                type='email'
+                autoComplete='email'
+                required
+                value={user.email}
+                className='block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor='password'
+              className='block text-sm font-medium leading-6 text-gray-900'
+            >
+              Password
+            </label>
+            <div className='mt-2'>
+              <input
+                id='password'
+                name='password'
+                type='password'
+                autoComplete='password'
+                required
+                value={user.password}
+                className='block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              disabled={buttonDisabled}
+              type='submit'
+              className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+            >
+              {buttonDisabled ? 'No Sign up' : 'Sign up'}
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <Link
+        className='mt-10 text-center text-sm text-indigo-600 hover:text-indigo-500'
+        href={'/login'}
+      >
+        Visit Login Page
+      </Link>
+    </div>
+  );
+}
